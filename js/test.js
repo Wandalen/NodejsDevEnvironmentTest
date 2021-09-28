d85866a27c514388ea717d9f0e2b6d301c5bb4fd
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-09-28 12:29:35 +0300

    update path

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 'use strict';
 const assert = require( 'assert' );
 const path = require( 'path' );
-const bindingPath = path.join( __dirname, './build/Release/binding' );
+const bindingPath = path.join( __dirname, '../build/Release/binding' );
 const binding = require( bindingPath );
 const status =  binding.status();
 assert.strictEqual( status, 'OK' );
